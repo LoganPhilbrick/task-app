e3eacfc3a6a0f7ed6b6f28266a40f67e070e80b5
WideLogan <logan@widecorpus.com>
2024-05-26 14:48:46 -0400

    fixed postgres returning stale data

--- app/api/get/route.js
+++ app/api/get/route.js
@@ -1,6 +1,8 @@
 import { sql } from "@vercel/postgres";
 import { NextResponse } from "next/server";
 
+export const fetchCache = "force-no-store";
+
 export async function GET(request) {
   const tasks = await sql`SELECT * FROM tasks`;
   return NextResponse.json(tasks.rows);
