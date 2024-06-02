0eea387a20c51a4bd291870ba454c2fda4f8590f
WideLogan <logan@widecorpus.com>
2024-06-02 12:46:25 -0400

    updated layout/ adjustments to address text wrap/overflow behavior

--- app/page.js
+++ app/page.js
@@ -6,6 +6,10 @@ import { useEffect, useState } from "react";
 
 export default function Home() {
   const [tasks, setTasks] = useState(null);
+  const completedTasks = [
+    { title: "Tasks title is too long to fit on single line", text: "Text for the task goes here and describes the task hhhhhhhhhhhhh.", date: "06/26/2024 11:50 pm" },
+    { title: "Tasks title", text: "Describes the task.", date: "06/26/2024 11:50 pm" },
+  ];
 
   useEffect(() => {
     if (tasks === null) {
@@ -21,9 +25,11 @@ export default function Home() {
     return <div>Loading...</div>;
   }
   return (
-    <main>
-      <FormCard setTasks={setTasks} />
-      <MappedCards tasks={tasks} setTasks={setTasks} />
+    <main className="flex flex-col items-center">
+      <div>
+        <FormCard setTasks={setTasks} />
+        <MappedCards tasks={tasks} setTasks={setTasks} done={completedTasks} />
+      </div>
     </main>
   );
 }
--- components/mappedCards.js
+++ components/mappedCards.js
@@ -2,10 +2,10 @@
 
 import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "./ui/button";
-import { Trash2 } from "lucide-react";
+import { Trash2, CircleCheckBig } from "lucide-react";
 import { cn } from "@/lib/utils";
 
-export default function MappedCards({ tasks, setTasks }) {
+export default function MappedCards({ tasks, setTasks, done }) {
   const deleteTask = async (id) => {
     if (id) {
       const res = await fetch(
@@ -23,24 +23,48 @@ export default function MappedCards({ tasks, setTasks }) {
   if (!tasks) {
     return <div>Loading...</div>;
   }
-  return tasks.map((task, id) => (
-    <div key={id} className="m-4">
-      <Card className="w-96 group">
-        <CardHeader className="flex flex-row">
-          <div>
-            <CardTitle className="">{task.title}</CardTitle>
-            <CardDescription className="text-gray-700 mt-1">{task.text}</CardDescription>
-            <CardDescription className="text-gray-400 mt-5">
-              created {task.date} {task.time}
-            </CardDescription>
+  return (
+    <div className="flex">
+      <div>
+        {tasks.map((task, id) => (
+          <div key={id} className="w-96 m-4">
+            <Card className="group">
+              <CardHeader className="flex flex-row">
+                <div className="w-5/6">
+                  <CardTitle>{task.title}</CardTitle>
+                  <CardDescription className="text-gray-700 mt-1 break-words">{task.text}</CardDescription>
+                  <CardDescription className="text-gray-400 mt-5">
+                    created {task.date} {task.time}
+                  </CardDescription>
+                </div>
+                <div className="ml-auto flex flex-col justify-center invisible group-hover:visible">
+                  <Button variant="outline" size="icon" onClick={() => deleteTask(task.id)}>
+                    <Trash2 className="h-[1.2rem] w-[1.2rem]" />
+                  </Button>
+                </div>
+              </CardHeader>
+            </Card>
           </div>
-          <div className="ml-auto flex flex-col justify-center invisible group-hover:visible">
-            <Button variant="outline" size="icon" onClick={() => deleteTask(task.id)}>
-              <Trash2 className="h-[1.2rem] w-[1.2rem]" />
-            </Button>
+        ))}
+      </div>
+      <div>
+        {done.map((task, id) => (
+          <div key={id} className="m-4 w-96">
+            <Card className="group ">
+              <CardHeader className="flex flex-row">
+                {/* ^^ style={{ filter: "blur(1px)" }} ^^ */}
+                <div className="w-5/6">
+                  <CardTitle className="truncate">{task.title}</CardTitle>
+                  <CardDescription className="text-gray-700 mt-1 truncate">{task.text}</CardDescription>
+                  <CardDescription className="text-gray-400 mt-5">
+                    created {task.date} {task.time}
+                  </CardDescription>
+                </div>
+              </CardHeader>
+            </Card>
           </div>
-        </CardHeader>
-      </Card>
+        ))}
+      </div>
     </div>
-  ));
+  );
 }
